3cab20a9238538049a1ccebb5c3521c51e7f753c
manuasdf <3185040+manuasdf@users.noreply.github.com>
2022-10-20 11:46:33 +0200

    Supplement routine

--- trainer.py
+++ trainer.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
 import sys
+import json
 
 # Import alphabets
+german = open("./alphabets/german.json")
+international = open("./alphabets/international.json")
+
+dataSet = json.load(german)
+
+
+def questionnaire():
+    for letter in dataSet
+    while True:
+        guess = input(dataSet['A'] + ": ")
+        if guess == "q":
+            exit()
+        else guess == dataSet['']:
+            return
+
+if __name__ == "__main__":
+    questionnaire()
